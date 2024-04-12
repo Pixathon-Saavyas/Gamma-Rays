6d8f35841f15c4b3dc65e82db929cf3a3e397850
Atharva Parkhe <patharv777@gmail.com>
2024-04-13 01:02:33 +0530

    something

---
+++ src/utils/db.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+# CONNECTION_STRING = "mongodb+srv://testuser:VNIeMDrYspPKtKRY@test1.cuzl9bm.mongodb.net/test1?retryWrites=true&w=majority"
+# client = MongoClient(CONNECTION_STRING)
+# db = client['test1']
+# collection = db['test1.cuzl9bm.mongodb.net']
+
+# data = {
+#     "question": "hi",
+#     "answer": "hello"
+# }
+# result = collection.insert_one(data)
+# print(f"Inserted document ID: {result.inserted_id}")
+
+# cursor = collection.find({})
+# for document in cursor:
+#     print(document)
+
+# client.close()
